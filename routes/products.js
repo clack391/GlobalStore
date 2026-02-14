9e72f609842b63c51adfd5bbdba7e4407eeecf50
Precious Onotu <clack391@gmail.com>
2026-02-14 03:11:52 +0100

    validation logic updated

--- routes/products.js
+++ routes/products.js
@@ -3,18 +3,8 @@ const router = express.Router();
 
 const productsController = require('../controllers/products');
 
-const { body } = require('express-validator');
 const validation = require('../middleware/validate');
-
-const validateProduct = [
-    body('name').notEmpty().withMessage('Name is required'),
-    body('price').isNumeric().withMessage('Price must be a number'),
-    body('description').notEmpty().withMessage('Description is required'),
-    body('category').notEmpty().withMessage('Category is required'),
-    body('stock').isInt().withMessage('Stock must be an integer'),
-    body('brand').notEmpty().withMessage('Brand is required'),
-    body('rating').isNumeric().withMessage('Rating must be a number')
-];
+const { validateProduct } = require('../middleware/validation');
 
 router.get('/', productsController.getAll);
 router.get('/:id', productsController.getSingle);
--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,8 @@ const router = express.Router();
 
 const usersController = require('../controllers/users');
 
-const { body } = require('express-validator');
 const validation = require('../middleware/validate');
-
-const validateUser = [
-    body('username').notEmpty().withMessage('Username is required'),
-    body('oauthId').notEmpty().withMessage('OAuth ID is required'),
-    body('avatar').optional().isString().withMessage('Avatar must be a string'),
-    body('role').optional().isString().withMessage('Role must be a string')
-];
+const { validateUser } = require('../middleware/validation');
 
 router.get('/', usersController.getAll);
 router.get('/:id', usersController.getSingle);
